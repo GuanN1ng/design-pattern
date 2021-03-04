6e309b7604eefc2bf45e4854dd9e600ff037a4de
GuanN1ng <guann1n9@gmail.com>
2021-03-04 19:38:52 +0800

    单例模式

---
+++ src/main/java/com/guann1n9/design/pattern/Behavioral/SingletonEnum.java
@@ -0,0 +1,23 @@
+package com.guann1n9.design.pattern.Behavioral;
+
+public enum SingletonEnum {
+
+    INSTANCE;
+
+
+    private String desc = "hello";
+
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(INSTANCE.getDesc());
+    }
+}
--- src/main/java/com/guann1n9/design/pattern/Singleton.java
+++
@@ -1,8 +0,0 @@
-package com.guann1n9.design.pattern;
-
-
-/**
- * 单例模式
- */
-public class Singleton {
-}
